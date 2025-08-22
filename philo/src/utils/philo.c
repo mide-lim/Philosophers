3e20a04dfae3a4a6189a0ba20749c3ca81259642
Michel de Lima <midelim.dev@gmail.com>
2025-08-22 00:00:50 +0000

    feat: creating mutexes in forks and testing routine with treads

--- philo/src/utils/philo.c
+++ philo/src/utils/philo.c
@@ -5,6 +5,20 @@ void    *philo_routine(void *arg)
     t_philo *philo;
 
     philo = (t_philo *)arg;
+
     printf("Philosopher %d started\n", philo->id);
+    pthread_mutex_lock(&philo->data->forks[philo->left_fork]);
+    printf("Philosopher %d picked up left fork %d\n", philo->id, philo->left_fork);
+
+    pthread_mutex_lock(&philo->data->forks[philo->right_fork]);
+    printf("Philosopher %d picked up right fork %d\n", philo->id, philo->right_fork);
+
+    printf("Philosopher %d is eating 🍝\n", philo->id);
+    ft_usleep(philo->data->time_to_eat);
+
+    pthread_mutex_unlock(&philo->data->forks[philo->left_fork]);
+    pthread_mutex_unlock(&philo->data->forks[philo->right_fork]);
+    printf("Philosopher %d released forks\n", philo->id);
+
     return (NULL);
 }
